511d447113857c7df6be88b3d57fdb26a782176b
ahmadalizadeh3215 <ahmadalizadem361218@gmail.com>
2022-05-20 12:25:12 +0430

    create project

---
+++ src/test/java/com/example/ProjectFinalMaktab_part3/ProjectFinalMaktabPart3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectFinalMaktab_part3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFinalMaktabPart3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
