5f954c9cb241338ffa233847c9279267e8663c1e
ahmadalizadeh3215 <ahmadalizadem361218@gmail.com>
2022-05-29 16:40:59 +0430

    spring boot application

---
+++ src/main/java/com/example/ProjectFinalMaktab_part3/project/ProjectFinalMaktabPart3Application.java
@@ -0,0 +1,13 @@
+package com.example.ProjectFinalMaktab_part3.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFinalMaktabPart3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFinalMaktabPart3Application.class, args);
+	}
+
+}
