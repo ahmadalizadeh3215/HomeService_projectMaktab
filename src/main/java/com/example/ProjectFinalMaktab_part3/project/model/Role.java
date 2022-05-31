e903e1197e4a06460e8f09b24f579adcd5ff72b8
ahmadalizadeh3215 <ahmadalizadem361218@gmail.com>
2022-05-31 16:15:13 +0430

    updated Role

--- src/main/java/com/example/ProjectFinalMaktab_part3/project/model/Role.java
+++ src/main/java/com/example/ProjectFinalMaktab_part3/project/model/Role.java
@@ -2,10 +2,9 @@ package com.example.ProjectFinalMaktab_part3.project.model;
 
 
 public enum Role {
-    ROLE_CUSTOMER,
-    ROLE_ADMIN,
-    ROLE_SPECIALIST;
-
+    CUSTOMER,
+    ADMIN,
+    SPECIALIST;
 
 
 }
