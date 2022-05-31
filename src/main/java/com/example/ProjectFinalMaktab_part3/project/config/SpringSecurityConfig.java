5230105d77469b93b03a9b8285adbd4a8e962155
ahmadalizadeh3215 <ahmadalizadem361218@gmail.com>
2022-05-31 16:49:52 +0430

    add authorize,add method BCryptPasswordEncoder,

--- src/main/java/com/example/ProjectFinalMaktab_part3/project/config/SpringSecurityConfig.java
+++ src/main/java/com/example/ProjectFinalMaktab_part3/project/config/SpringSecurityConfig.java
@@ -1,11 +1,15 @@
 package com.example.ProjectFinalMaktab_part3.project.config;
 
+import com.example.ProjectFinalMaktab_part3.project.model.Users;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 import javax.sql.DataSource;
 
@@ -14,22 +18,32 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private DataSource dataSource;
+    private Users users;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().antMatchers("/", "/index")
+                .authorizeRequests().antMatchers( "/","/index","/css/**")
                 .permitAll()
+                .antMatchers("/users","/customers")
+                .hasAuthority("ADMIN")
                 .anyRequest().authenticated()
                 .and().formLogin()
-                .loginPage("/login").usernameParameter("email").permitAll().and().logout().permitAll();
+                .loginPage("/login").usernameParameter("email")
+                .permitAll().and().logout().permitAll();
     }
-
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication().dataSource(dataSource)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .passwordEncoder(bCryptPasswordEncoder())
                 .usersByUsernameQuery("select email,password,enabled from users where email=?")
-                .authoritiesByUsernameQuery("select email,role from authoritises where email=?");
+                .authoritiesByUsernameQuery("select email,role from authoritises where email=?")
+              ;
     }
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
 }
