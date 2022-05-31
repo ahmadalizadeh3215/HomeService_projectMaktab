44c687a4a7f911c3407fade9317f39cd2e792776
ahmadalizadeh3215 <ahmadalizadem361218@gmail.com>
2022-05-31 16:47:14 +0430

    add class application

--- src/main/java/com/example/ProjectFinalMaktab_part3/project/ProjectFinalMaktabPart3Application.java
+++ src/main/java/com/example/ProjectFinalMaktab_part3/project/ProjectFinalMaktabPart3Application.java
@@ -2,12 +2,16 @@ package com.example.ProjectFinalMaktab_part3.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ProjectFinalMaktabPart3Application {
 
 	public static void main(String[] args) {
+		System.out.println("1111");
+		System.out.println(new BCryptPasswordEncoder().encode("1111"));
 		SpringApplication.run(ProjectFinalMaktabPart3Application.class, args);
+
 	}
 
 }
