0f7af4ea91da07c809693356554dd0c700982bdc
ahmadalizadeh3215 <ahmadalizadem361218@gmail.com>
2022-06-03 14:05:52 +0430

    implements GrantedAuthority

--- src/main/java/com/example/ProjectFinalMaktab_part3/project/model/Role.java
+++ src/main/java/com/example/ProjectFinalMaktab_part3/project/model/Role.java
@@ -1,10 +1,16 @@
 package com.example.ProjectFinalMaktab_part3.project.model;
 
 
-public enum Role {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
     CUSTOMER,
     ADMIN,
     SPECIALIST;
 
 
+    @Override
+    public String getAuthority() {
+        return this.name();
+    }
 }
