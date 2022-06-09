ad742aef4a19717b14b7b7a5800bd8055df55b34
ahmadalizadeh3215 <ahmadalizadem361218@gmail.com>
2022-06-09 21:13:03 +0430

    add bean modelMapper

--- src/main/java/com/example/ProjectFinalMaktab_part3/project/ProjectFinalMaktabPart3Application.java
+++ src/main/java/com/example/ProjectFinalMaktab_part3/project/ProjectFinalMaktabPart3Application.java
@@ -1,15 +1,20 @@
 package com.example.ProjectFinalMaktab_part3.project;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class ProjectFinalMaktabPart3Application {
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
-		System.out.println("1111");
-		System.out.println(new BCryptPasswordEncoder().encode("1111"));
+
 		SpringApplication.run(ProjectFinalMaktabPart3Application.class, args);
 
 	}
