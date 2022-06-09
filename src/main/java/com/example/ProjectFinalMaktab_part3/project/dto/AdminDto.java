cda4fce6d53fc88d8569cd2ec957cc0781b94c3f
ahmadalizadeh3215 <ahmadalizadem361218@gmail.com>
2022-06-09 20:53:15 +0430

    insert package dto

---
+++ src/main/java/com/example/ProjectFinalMaktab_part3/project/dto/AdminDto.java
@@ -0,0 +1,10 @@
+package com.example.ProjectFinalMaktab_part3.project.dto;
+
+import lombok.Data;
+import javax.persistence.DiscriminatorValue;
+
+@Data
+@DiscriminatorValue("ADMIN")
+public class AdminDto extends UserDto{
+
+}
---
+++ src/main/java/com/example/ProjectFinalMaktab_part3/project/dto/SpecialistDto.java
@@ -0,0 +1,25 @@
+package com.example.ProjectFinalMaktab_part3.project.dto;
+
+import com.example.ProjectFinalMaktab_part3.project.model.SubTask;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+
+@Data
+@DiscriminatorValue("SPECIALIST")
+@ToString
+public class SpecialistDto extends UserDto{
+    private String skill;
+    private Integer score;
+    private Double validity;
+    @ManyToOne
+    private SubTask subTask;
+    @Lob
+    private byte[] personalPhoto;
+}
