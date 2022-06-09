41509516f876d8ee8044cf512b38427e465a1d59
ahmadalizadeh3215 <ahmadalizadem361218@gmail.com>
2022-06-10 03:31:01 +0430

    add repository class

---
+++ src/main/java/com/example/ProjectFinalMaktab_part3/project/repository/OnlinePaymentRepository.java
@@ -0,0 +1,7 @@
+package com.example.ProjectFinalMaktab_part3.project.repository;
+
+import com.example.ProjectFinalMaktab_part3.project.model.OnlinePayment;
+
+public interface OnlinePaymentRepository extends GenericRepository<OnlinePayment,Long> {
+
+}
