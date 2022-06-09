e0ce8b1e66f047f9425c127b421083b9e51d8454
ahmadalizadeh3215 <ahmadalizadem361218@gmail.com>
2022-06-09 20:43:05 +0430

    add configure and set authorize

--- src/main/java/com/example/ProjectFinalMaktab_part3/project/config/SpringSecurityConfig.java
+++ src/main/java/com/example/ProjectFinalMaktab_part3/project/config/SpringSecurityConfig.java
@@ -1,48 +1,54 @@
 package com.example.ProjectFinalMaktab_part3.project.config;
 
-import com.example.ProjectFinalMaktab_part3.project.model.Users;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.context.annotation.Bean;
+import com.example.ProjectFinalMaktab_part3.project.service.Impl.UserServiceImpl;
+
+
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
-import javax.sql.DataSource;
-
 @Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private DataSource dataSource;
-    private Users users;
+    private final UserServiceImpl userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+
+    public SpringSecurityConfig( UserServiceImpl userService,BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userService = userService;
+        this.bCryptPasswordEncoder=bCryptPasswordEncoder;
+
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().antMatchers( "/","/index","/css/**")
-                .permitAll()
-                .antMatchers("/users","/customers")
-                .hasAuthority("ADMIN")
+                .authorizeRequests()
+                .antMatchers("/", "/index", "/login","/registrationUser/**").permitAll()
+           .antMatchers("/admin/**").hasAnyAuthority("ADMIN")
+                .antMatchers("/customer/**").hasAnyAuthority("ADMIN", "CUSTOMER")
+                .antMatchers("/specialist/**").hasAnyAuthority("SPECIALIST", "ADMIN")
+                .antMatchers("/tasks/**").hasAnyAuthority("SPECIALIST", "ADMIN")
+                .antMatchers("/subtask/**").hasAnyAuthority("SPECIALIST", "ADMIN")
+                .antMatchers("/orders/**").permitAll()
+                .antMatchers("/offers/**").permitAll()
                 .anyRequest().authenticated()
-                .and().formLogin()
-                .loginPage("/login").usernameParameter("email")
-                .permitAll().and().logout().permitAll();
+                .and().formLogin().loginPage("/login").usernameParameter("email");
+
     }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication().dataSource(dataSource)
-                .passwordEncoder(bCryptPasswordEncoder())
-                .usersByUsernameQuery("select email,password,enabled from users where email=?")
-                .authoritiesByUsernameQuery("select email,role from authoritises where email=?")
-              ;
-    }
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+        auth.userDetailsService(userService );
     }
 
 
