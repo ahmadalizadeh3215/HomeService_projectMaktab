0e078ace04e4b08c45c8a871e0f0cbf4c95bbea0
ahmadalizadeh3215 <ahmadalizadem361218@gmail.com>
2022-06-16 00:17:41 +0430

    add endpoint showOrderstospecilist,gridSearch

--- src/main/java/com/example/ProjectFinalMaktab_part3/project/controller/OrderController.java
+++ src/main/java/com/example/ProjectFinalMaktab_part3/project/controller/OrderController.java
@@ -1,25 +1,19 @@
 package com.example.ProjectFinalMaktab_part3.project.controller;
 
-import com.example.ProjectFinalMaktab_part3.project.dto.SpecialistDto;
-import com.example.ProjectFinalMaktab_part3.project.model.Customer;
-import com.example.ProjectFinalMaktab_part3.project.model.Orders;
-import com.example.ProjectFinalMaktab_part3.project.model.Role;
-import com.example.ProjectFinalMaktab_part3.project.model.Specialist;
-import com.example.ProjectFinalMaktab_part3.project.registration.PasswordEncoder;
-import com.example.ProjectFinalMaktab_part3.project.service.Impl.CustomerServiceImpl;
-import com.example.ProjectFinalMaktab_part3.project.service.Impl.OrdersServiceImpl;
-import com.example.ProjectFinalMaktab_part3.project.service.Impl.SpecialistServiceImpl;
-import com.example.ProjectFinalMaktab_part3.project.service.Impl.UserServiceImpl;
-import org.hibernate.criterion.Order;
-import org.modelmapper.ModelMapper;
+
+import com.example.ProjectFinalMaktab_part3.project.model.*;
+import com.example.ProjectFinalMaktab_part3.project.service.Impl.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Controller
@@ -27,28 +21,27 @@ import java.util.List;
 public class OrderController {
     private Orders orders;
     private OrdersServiceImpl ordersService;
-    private Customer customer;
     private CustomerServiceImpl customerService;
     private SpecialistServiceImpl specialistService;
     private UserServiceImpl userService;
+    private SubTaskServiceImpl subTaskService;
 
 
     public OrderController(Orders orders, OrdersServiceImpl ordersService,
-                           Customer customer, CustomerServiceImpl customerService,
+                           CustomerServiceImpl customerService,
                            SpecialistServiceImpl specialistService,
-                           UserServiceImpl userService
-                           ) {
+                           UserServiceImpl userService, SubTaskServiceImpl subTaskService
+    ) {
         this.orders = orders;
         this.ordersService = ordersService;
-        this.customer = customer;
         this.customerService = customerService;
         this.specialistService = specialistService;
         this.userService = userService;
+        this.subTaskService = subTaskService;
 
     }
 
     private final Logger LOG = LoggerFactory.getLogger(SpecialistController.class);
-
 
 
     @GetMapping("/findAll")
@@ -57,26 +50,41 @@ public class OrderController {
         return ResponseEntity.ok(ordersService.findAll());
     }
 
+
     @GetMapping(value = "/findById/{id}")
     public ResponseEntity<Orders> get(@PathVariable("id") Integer id) {
         LOG.info("getting Orders with id: {}", id);
-        Orders orders= ordersService.findById(id).orElseThrow(()->
-                 new IllegalStateException("user with id {} not found"+id));
+        Orders orders = ordersService.findById(id).orElseThrow(() ->
+                new IllegalStateException("user with id {} not found" + id));
 
         return ResponseEntity.ok().body(orders);
     }
 
-    @PostMapping(value = "/saveOrders")
-    public ResponseEntity<Orders> create(@RequestBody @Valid Orders orders) {
-Orders orders1=ordersService.save(orders);
-return ResponseEntity.ok(orders1);
+    @PostMapping(value = "/saveOrders/{customerEmail}/{subTaskId}/{dateWork}")
+    public ResponseEntity<Orders> create(@RequestBody  Orders orders,
+                                         @PathVariable("customerEmail") String customerEmail,
+                                         @PathVariable("subTaskId") Integer subTaskId,
+                                         @PathVariable("dateWork")Integer dateWork) {
+        System.out.println(customerEmail);
+        System.out.println(subTaskId);
+        System.out.println(dateWork);
+        Customer customer1 = customerService.findByEmail(customerEmail);
+        SubTask subTask = subTaskService.findById(subTaskId).orElseThrow(()
+                -> new IllegalStateException("subTask id not found !"));
+        orders.setCustomer(customer1);
+        orders.setSubTask(subTask);
+        orders.setOrderStatus(OrderStatus.Waiting_For_Expert_Suggestions);
+        orders.setDateOrderRegistration(LocalDateTime.now());
+       orders.setDateTimeWork(LocalDateTime.now().plus(dateWork, ChronoUnit.DAYS));
+        Orders orders1 = ordersService.save(orders);
+        return ResponseEntity.ok(orders1);
     }
 
     @PutMapping(value = "/update/{id}")
-    public  ResponseEntity<Orders> update(@PathVariable("id")Integer id, @RequestBody Orders orders) {
+    public ResponseEntity<Orders> update(@PathVariable("id") Integer id, @RequestBody Orders orders) {
 
         LOG.info("updating orders: {}", orders);
-       Orders orders1 = ordersService.findById(id).orElseThrow();
+        Orders orders1 = ordersService.findById(id).orElseThrow();
         if (orders1 == null) {
             LOG.info("orders with id {} not found", id);
             return new ResponseEntity<Orders>(HttpStatus.NOT_FOUND);
@@ -104,17 +112,38 @@ return ResponseEntity.ok(orders1);
             return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
         }
 
-      ordersService.delete(orders);
+        ordersService.delete(orders);
         return new ResponseEntity<Void>(HttpStatus.OK);
     }
 
     @PutMapping(value = "/paymentByCredit/{id}")
-    ResponseEntity<Orders> paymentByCredit(@PathVariable("id")Integer id, @RequestBody Orders orders) throws Exception {
+    ResponseEntity<Orders> paymentByCredit(@PathVariable("id") Integer id, @RequestBody Orders orders) throws Exception {
         LOG.info("updating orders: {}", orders);
-      Orders order=ordersService.findById(id).orElseThrow(()->
+        Orders order = ordersService.findById(id).orElseThrow(() ->
                 new IllegalStateException("order not found"));
-      ordersService.PaymentCredit(order);
+        ordersService.PaymentCredit(order);
         return new ResponseEntity<Orders>(order, HttpStatus.OK);
+    }
+
+
+    @GetMapping(value = "/showOrdersToSpecialist/{specialistEmail}")
+    public ResponseEntity<List<Orders>> showOrdersToSpecialist(@RequestBody Orders orders ,
+                                                               @PathVariable("specialistEmail")String specialistEmail){
+        System.out.println(specialistEmail);
+        Specialist specialist=specialistService.findByEmail(specialistEmail);
+        List<Orders> ordersList=ordersService.showOrderSpecialist(specialistEmail);
+
+        return new  ResponseEntity<List<Orders>>(ordersList,HttpStatus.OK);
+    }
+    @GetMapping(value = "/gridSreach/")
+    public ResponseEntity<List<Orders>> gridSreach(@RequestParam(required = false, name = "proposedPrice") Double proposedPrice,
+                                                  @RequestParam(required = false, name = "subTask") String subTask,
+                                                  @RequestParam(required = false, name = "dateTimeWork") LocalDateTime dateTimeWork,
+                                                  @RequestParam(required = false, name = "orderStatus") OrderStatus orderStatus) {
+
+        List<Orders> ordersList = ordersService.gridSearchOrders(proposedPrice, subTask, dateTimeWork, orderStatus);
+
+        return new ResponseEntity<List<Orders>>(ordersList,HttpStatus.OK);
     }
 
 
