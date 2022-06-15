5fa361519ad3ef3d496ecabf8b5b8e8818edbd82
ahmadalizadeh3215 <ahmadalizadem361218@gmail.com>
2022-06-16 00:19:13 +0430

    bugFix

--- src/main/java/com/example/ProjectFinalMaktab_part3/project/controller/CommentController.java
+++ src/main/java/com/example/ProjectFinalMaktab_part3/project/controller/CommentController.java
@@ -1,9 +1,47 @@
 package com.example.ProjectFinalMaktab_part3.project.controller;
 
+import com.example.ProjectFinalMaktab_part3.project.model.*;
+import com.example.ProjectFinalMaktab_part3.project.service.Impl.CommentServiceImpl;
+import com.example.ProjectFinalMaktab_part3.project.service.Impl.CustomerServiceImpl;
+import com.example.ProjectFinalMaktab_part3.project.service.Impl.SpecialistServiceImpl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/comments")
 public class CommentController {
+    private SpecialistServiceImpl specialistService;
+    private CustomerServiceImpl customerService;
+    private CommentServiceImpl commentService;
+    private Comment comment;
+
+    public CommentController(SpecialistServiceImpl specialistService,
+                             CustomerServiceImpl customerService,
+                             CommentServiceImpl commentService,
+                             Comment comment) {
+        this.specialistService = specialistService;
+        this.customerService = customerService;
+        this.commentService = commentService;
+        this.comment=comment;
+    }
+
+    @PostMapping(value = "/setComment/{specialistEmail}/{customerEmail}")
+    public ResponseEntity<Comment> create(@RequestBody Comment comment,
+                                          @PathVariable("specialistEmail") String specialistEmail,
+                                          @PathVariable("customerEmail") String customerEmail) {
+        System.out.println(specialistEmail);
+        System.out.println(customerEmail);
+        Specialist specialist = specialistService.findByEmail(specialistEmail);
+        Customer customer = customerService.findByEmail(customerEmail);
+        comment.setSpecialist(specialist);
+        comment.setCustomer(customer);
+        commentService.save(comment);
+        return ResponseEntity.ok(comment);
+    }
+
+
 }
--- src/main/java/com/example/ProjectFinalMaktab_part3/project/controller/SubTaskController.java
+++ src/main/java/com/example/ProjectFinalMaktab_part3/project/controller/SubTaskController.java
@@ -1,14 +1,14 @@
 package com.example.ProjectFinalMaktab_part3.project.controller;
 
+import com.example.ProjectFinalMaktab_part3.project.model.Specialist;
 import com.example.ProjectFinalMaktab_part3.project.model.SubTask;
 import com.example.ProjectFinalMaktab_part3.project.model.Tasks;
+import com.example.ProjectFinalMaktab_part3.project.service.Impl.SpecialistServiceImpl;
 import com.example.ProjectFinalMaktab_part3.project.service.Impl.SubTaskServiceImpl;
 import com.example.ProjectFinalMaktab_part3.project.service.Impl.TaskServiceImpl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -19,18 +19,33 @@ public class SubTaskController {
     private SubTaskServiceImpl subTaskService;
     private Tasks tasks;
     private TaskServiceImpl taskService;
+    private Specialist specialist;
+    private SpecialistServiceImpl specialistService;
 
     public SubTaskController(SubTaskServiceImpl subTaskService,
                              Tasks tasks,
-                             TaskServiceImpl taskService) {
+                             TaskServiceImpl taskService,
+                             Specialist specialist,
+                             SpecialistServiceImpl specialistService) {
         this.subTaskService = subTaskService;
         this.tasks = tasks;
         this.taskService = taskService;
+        this.specialist=specialist;
+        this.specialistService=specialistService;
+
     }
 
-    @PostMapping(value = "/save")
-    public ResponseEntity<SubTask> create(@RequestBody @Valid SubTask subTask) {
-       SubTask subTask1=subTaskService.save(subTask);
-        return ResponseEntity.ok(subTask1);
+    @PostMapping(value = "/save/{id}/{email}")
+    public ResponseEntity<SubTask> create( @RequestBody SubTask subTask,
+                                           @PathVariable("id") Integer id,
+                                           @PathVariable("email") String email) {
+        Tasks tasks1 = taskService.findById(id).orElseThrow(()
+                -> new IllegalStateException("tasks not found ! "));
+       Specialist specialist1=specialistService.findByEmail(email);
+        subTask.setTasks(tasks1);
+        subTask.setSpecialist(specialist1);
+        subTaskService.save(subTask);
+        return ResponseEntity.ok(subTask);
     }
+
 }
