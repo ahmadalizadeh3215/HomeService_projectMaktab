904e8878363a4be5b7cd4eb68ed255f11cab6e3f
ahmadalizadeh3215 <ahmadalizadem361218@gmail.com>
2022-06-16 00:04:15 +0430

    add method findAll as type Specification

--- src/main/java/com/example/ProjectFinalMaktab_part3/project/repository/OrdersRepository.java
+++ src/main/java/com/example/ProjectFinalMaktab_part3/project/repository/OrdersRepository.java
@@ -1,20 +1,27 @@
 package com.example.ProjectFinalMaktab_part3.project.repository;
 
 import com.example.ProjectFinalMaktab_part3.project.model.Orders;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface OrdersRepository extends GenericRepository<Orders, Integer> {
+public interface OrdersRepository extends GenericRepository<Orders, Integer>, JpaSpecificationExecutor<Orders> {
     Orders findBySubTaskName(String subTaskName);
 
     @Query("select o from SubTask as st INNER JOIN Specialist as s on s.id=st.specialist.id" +
             " JOIN Orders as o on st.id = o.subTask.id where st.specialist.id = :specialist_id")
     List<Orders> showOrdersToSpecialist(@Param("specialist_id") Integer specialist_id);
 
+    @Query("select o from SubTask as st inner join Specialist as s on s.email=st.specialist.email" +
+            " join Orders as o on st.id=o.subTask.id where st.specialist.email= :specialist_email")
+    List<Orders> showOrderSpecialist(@Param("specialist_email")String specialist_email);
+
     List<Orders> findByCustomerId(Integer customerId);
     Orders findByOrderStatus(String orderStatus);
+    List<Orders> findAll(Specification<Orders> specification);
 
 
 
--- src/main/java/com/example/ProjectFinalMaktab_part3/project/repository/UserRepository.java
+++ src/main/java/com/example/ProjectFinalMaktab_part3/project/repository/UserRepository.java
@@ -3,15 +3,19 @@ package com.example.ProjectFinalMaktab_part3.project.repository;
 import com.example.ProjectFinalMaktab_part3.project.model.Customer;
 import com.example.ProjectFinalMaktab_part3.project.model.Users;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
 import java.util.Optional;
 
 
-public interface UserRepository extends GenericRepository<Users, Integer> {
+public interface UserRepository extends GenericRepository<Users, Integer>, JpaSpecificationExecutor<Users> {
     Users findByEmailAndPassword(String email, String password);
 
     Optional<Users> findByEmail(String email);
     Boolean existsByEmail(String email);
+    List<Users> findAll(Specification<Users> specification);
 
 
 
